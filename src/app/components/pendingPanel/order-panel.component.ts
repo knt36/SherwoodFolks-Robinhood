840d982b59df471df84903747731facc1b8fd524
Anh Le <aql25@drexel.edu>
2017-10-29 02:00:11 -0400

    [feature-UI] add tooltip to indicate order status / order panel

--- src/app/components/pendingPanel/order-panel.component.ts
+++ src/app/components/pendingPanel/order-panel.component.ts
@@ -21,14 +21,47 @@ export class OrderPanelComponent{
     symbol: 'SQ',
     type: 'market buy',
     avg_cost: '32.43',
-    quantity: '23'
+    quantity: '23',
+    status: 'Pending',
+    color: 'gray'
   },
     {
       symbol: 'SQ',
       type: 'market buy',
       avg_cost: '32.43',
-      quantity: '23'
+      quantity: '23',
+      status: 'Pending',
+      color: 'gray'
     }];
+
+  recentOrder = [
+    {
+      symbol: 'SQ',
+      type: 'market buy',
+      avg_cost: '32.43',
+      quantity: '23',
+      status: 'Filled',
+      color: 'green'
+    },{
+      symbol: 'SQ',
+      type: 'market buy',
+      avg_cost: '32.43',
+      quantity: '23',
+      status: 'Canceled',
+      color: 'brown'
+    },
+  ];
+
+  legend:any = {
+    queued : {name: "Pending" , color: "gray"},
+    unconfirmed : {name: "Pending", color: "gray"},
+    confirmed : {name: "Pending", color: "gray"},
+    partially_filled : {name: "Partially filled", color: "yellow"},
+    filled : {name: "Filled", color: "green"},
+    rejected : {name: "Rejected", color: "red"},
+    canceled : {name: "Canceled", color: "brown"},
+    failed : {name: "Failed", color: "orange"},
+  }
 
   constructor(public rb:RobinhoodService){
 
